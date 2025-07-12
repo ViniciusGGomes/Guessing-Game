15b6fbcdd09af90608fdce3674141b196bc81afc
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-07-12 19:29:54 +0000

    feat: add LetterUsed component with styles

---
+++ src/module/LettersUsed/index.tsx
@@ -0,0 +1,29 @@
+import styles from "./styles.module.css";
+import { Letter } from "../Letter";
+
+export type LetterUsedProps = {
+  value: string;
+  correct: boolean;
+};
+
+type Props = {
+  data: LetterUsedProps[];
+};
+
+export function LetterUsed({ data }: Props) {
+  return (
+    <div className={styles.letterUser}>
+      <h5>Letras Utilizadas</h5>
+
+      <div>
+        {data.map(({ value, correct }) => (
+          <Letter
+            value={value}
+            size="small"
+            color={correct ? "correct" : "wrong"}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
